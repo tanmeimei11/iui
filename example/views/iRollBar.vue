5697c666314cacc954fbbc6c59279f986c61ffaf
diandi <diandi@in66.com>
2017-02-22 18:33:43 +0800

    add iscroll

--- example/views/iRollBar.vue
+++ example/views/iRollBar.vue
@@ -1,27 +1,42 @@
 <template>
   <article>
-    <iRollBar class="roll-bar" tag="ul" hidden-bar="false">
-       <li v-for="item in items">{{item}}</li>
+    <iRollBar class="roll-bar" tag="ul" :rollIng="roll" showBar >
+      <li v-for="item in items">{{item}}</li>
     </iRollBar>
   </article>
 </template>
 <script>
-export default {
-  name: 'i-roll-bar',
-  data () {
-    return {
-      items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+  export default {
+    name: 'i-roll-bar',
+    data () {
+      return {
+        items: []
+      }
+    },
+    created () {
+      for (var idx = 0; idx <= 35; idx++) {
+        this.items.push(idx)
+      }
+    },
+    methods: {
+      roll (cb) {
+        console.log(Date.now())
+        for (var idx = 0; idx <= 10; idx++) {
+          this.items.push(idx)
+        }
+        cb()
+      }
     }
   }
-}
+
 </script>
 <style lang="scss">
-  .roll-bar{
-     width: 400px;
-     height: 200px;
-  }
+  // .roll-bar{
+  //    width: 400px;
+  //    height: 200px;
+  // }
   code {
     margin-top: 20px;
   }
-</style>
 
+</style>
--- src/components/iRollBar/iRollBar.vue
+++ src/components/iRollBar/iRollBar.vue
@@ -3,19 +3,51 @@
   export default {
     name: 'i-roll-bar',
     props: {
-      hiddenBar: {
-        type: String,
-        default: 'true'
-      },
       tag: {
         type: String,
         default: 'div'
+      },
+      // debounce: {
+      //   type: Number,
+      //   default: 1000
+      // },
+      rollIng: {
+        type: Function,
+        default: () => {}
+      },
+      showBar: {
+        type: String
+      }
+    },
+    mounted () {
+      this.lock = undefined
+      window.lib = window.lib || { }
+      window.lib.flexible = window.lib.flexible || {dpr: 1}
+      window.lib.flexible.dpr = window.lib.flexible.dpr || 1
+      window.addEventListener('scroll', this.scrollHandler, false)
+    },
+    destroyed () {
+      window.removeEventListener('scroll', this.scrollHandler)
+    },
+    methods: {
+      scrollHandler (event) {
+        if (this.$el === undefined) return
+        if (this.lock !== undefined) return
+        let elH = this.$el.clientHeight
+        let elOffset = this.$el.getBoundingClientRect()
+        let screeH = window.screen.height * window.lib.flexible.dpr
+        let elBottomOffset = elOffset.top + elH
+        if (elBottomOffset < (screeH + 50)) {
+          this.lock = 1
+          this.rollIng(() => {
+            this.lock = undefined
+          })
+        }
       }
     },
     render (h) {
-      debugger
       const data = {
-        // class: `i-roll-bar ${Boolean(this.hiddenBar) ? 'hiddenBar' : ''}`
+        class: `i-roll-bar ${(this.showBar === undefined) ? 'hiddenBar' : ''}`
       }
       return h(this.tag, data, this.$slots.default)
     }
