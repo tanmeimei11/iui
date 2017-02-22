41d980883067c2ab4f48332e174e4b9a651c07f0
diandi <diandi@in66.com>
2017-02-22 13:43:19 +0800

    irollbar temp

---
+++ example/views/iRollBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <article>
+    <iRollBar class="roll-bar" tag="ul" hidden-bar="false">
+       <li v-for="item in items">{{item}}</li>
+    </iRollBar>
+  </article>
+</template>
+<script>
+export default {
+  name: 'i-roll-bar',
+  data () {
+    return {
+      items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    }
+  }
+}
+</script>
+<style lang="scss">
+  .roll-bar{
+     width: 400px;
+     height: 200px;
+  }
+  code {
+    margin-top: 20px;
+  }
+</style>
+
---
+++ src/components/iRollBar/iRollBar.vue
@@ -0,0 +1,32 @@
+
+<script>
+  export default {
+    name: 'i-roll-bar',
+    props: {
+      hiddenBar: {
+        type: String,
+        default: 'true'
+      },
+      tag: {
+        type: String,
+        default: 'div'
+      }
+    },
+    render (h) {
+      debugger
+      const data = {
+        // class: `i-roll-bar ${Boolean(this.hiddenBar) ? 'hiddenBar' : ''}`
+      }
+      return h(this.tag, data, this.$slots.default)
+    }
+  }
+</script>
+<style lang="scss">
+  .i-roll-bar{
+     -webkit-overflow-scrolling: touch;
+     overflow: auto;
+     &.hiddenBar::-webkit-scrollbar {
+       display: none;
+     }
+  }
+</style>
