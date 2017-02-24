703fbafa57d8f4fa17a8ab3cac1eb4cc7034c712
diandi <diandi@in66.com>
2017-02-24 18:46:00 +0800

    add new release

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,16 +10,16 @@ import { exec } from 'child_process'
 // minor: 0.1.0
 // patch: 0.0.2
 let newVersion
-let versionType = 'patch' 
-let run = (command, cb) => { exec(command, (err) => { if (err) return cb(err); cb() }) } 
+let versionType = 'patch'
+let run = (command, cb) => { exec(command, (err) => { if (err) return cb(err); cb() }) }
 
 gulp.task('npm:run:build', (cb) => {
-  run('npm run build', cb)   
+  run('npm run build', cb)
 })
 
 gulp.task('bump', ['npm:run:build'], () => {
   let packageJson = () => { return JSON.parse(fs.readFileSync('./package.json', 'utf8')) }
-  let json = packageJson()    
+  let json = packageJson()
   newVersion = semver.inc(json.version, versionType)
   gulp.src('./package.json')
     .pipe(bump({ version: newVersion, type: versionType }))
@@ -27,19 +27,19 @@ gulp.task('bump', ['npm:run:build'], () => {
 })
 
 gulp.task('git:tag', ['bump'], (cb) => {
-  run(`git tag -a ${newVersion} -m "Tagging ${newVersion}"`, cb)   
+  run(`git tag -a ${newVersion} -m "Tagging ${newVersion}"`, cb)
 })
 
 gulp.task('git:push:tags', ['git:tag'], (cb) => {
-  run('git push --tags', cb) 
+  run('git push --tags', cb)
 })
 
 gulp.task('copy:to:in', ['git:push:tags'], (cb) => {
-  run('rm -rf ../InPromo/2017/in-ui/dist/demo && cp -rf ./dist/demo ../InPromo/2017/in-ui/dist/', cb) 
+  run('rm -rf ../InPromo/2017/in-ui/dist/demo && cp -rf ./dist/demo ../InPromo/2017/in-ui/dist/', cb)
 })
 
 gulp.task('npm:publish', ['copy:to:in'], (cb) => {
-  run('npm publish', cb) 
+  run('npm publish', cb)
 })
 
 gulp.task('version:minor', () => { versionType = 'minor' })
