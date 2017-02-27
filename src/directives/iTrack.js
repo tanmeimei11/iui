0b2d332dee878f1b09be9e1004733da40855279b
maifei <maifei@in66.com>
2017-02-27 16:01:23 +0800

    hotfix: modified:   src/directives/iTrack.js

--- src/directives/iTrack.js
+++ src/directives/iTrack.js
@@ -1,23 +1,28 @@
 import track from '../util/track'
-const directive = (el, binding, v) => {
-  el.onclick = (e) => {
-    let items = []
-    let search = binding.value.trim() 
-    if (!search.length) return true
-    let [seed, query] = search.split('?')
-    if (query && query.length) { 
-      items = query.split('&')
-    }
-    track(seed, items)
+
+const clickCallback = function (e) {
+  let items = []
+  let search = this.value.trim() 
+  console.log(search)
+  if (!search.length) return true
+  let [seed, query] = search.split('?')
+  if (query && query.length) { 
+    items = query.split('&')
   }
+  track(seed, items)
 }
-const unbind = (el, binding, config) => {
-  el.removeAttribute('onclick')
+
+const directive = (el, binding, _v) => {
+  el.addEventListener('click', clickCallback.bind(binding)) 
+}
+
+const unbind = (el, binding, _v) => {
+  el.removeEventListener('onclick', clickCallback.bind(binding))
 }
 
 export default {
   bind: directive,
-  updated: directive,
-  componentUpdated: directive,
+  // updated: directive,
+  // componentUpdated: directive,
   unbind: unbind
 }
