c0c44ac78b9e0f63528005c67686a49231422dd7
maifei <maifei@in66.com>
2017-02-27 11:47:21 +0800

    modified:   example/views/changeLog.vue

--- example/views/changeLog.vue
+++ example/views/changeLog.vue
@@ -1,42 +1,5 @@
 <template>
   <article>
-	<section class="card-item default">
-      <r-card class="green accent-3">
-        <r-card-text>
-		  <p> version: 0.0.7 </p>
-		  <p> add component iAvatar</p>
-        </r-card-text>
-      </r-card>
-    </section>
-
-   <section class="card-item default">
-      <r-card class="green accent-3">
-        <r-card-text>
-		  <p> version: 0.0.5 </p>
-		  <p> improve directive iTrack, support query params</p>
-        </r-card-text>
-      </r-card>
-    </section>
-
-
-   <section class="card-item default">
-      <r-card class="green accent-3">
-        <r-card-text>
-		  <p> version: 0.0.3 </p>
-		  <p>1. add directive iTrack </p>
-		  <p>2. add util common.js </p>
-		  <p>3. add util track.js </p>
-        </r-card-text>
-      </r-card>
-    </section>
-
-   <section class="card-item default">
-      <r-card class="green accent-3">
-        <r-card-text>
-		  <p> version: 0.0.2</p>
-		  <span> add component iAudio </span>
-        </r-card-text>
-      </r-card>
-    </section>
+	http://githost.in66.cc/maifei/iui/milestones?state=all
   </article>
 </template>
