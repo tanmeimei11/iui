0c9df9f4e909181eac9db0463f6e05213f5382fb
diandi <diandi@in66.com>
2017-02-28 15:16:01 +0800

    fix barrage

--- example/views/iBarrage.vue
+++ example/views/iBarrage.vue
@@ -1,30 +1,6 @@
 <template>
   <article>
-    <iBarrage :list="[{
-          txt: '~我卡咔咔',
-          fontSize: '11px',
-          color: '#000',
-          offsetX: 0,
-          offsetY: 16,
-          speed: 1,
-          time: 1
-        }, {
-          txt: '你踩踩~',
-          fontSize: '11px',
-          color: '#000',
-          offsetX: 32,
-          offsetY: 30,
-          speed: 1,
-          time: 2
-        }, {
-          txt: '我是弹幕~',
-          fontSize: '11px',
-          color: '#000',
-          offsetX: 80,
-          offsetY: 100,
-          speed: 2,
-          time: Infinity
-        }]"></iBarrage>
+    <iBarrage :list="list" :lane="[100,10,250,200,225,150,175]" @done="doneBarrage"></iBarrage>
     <h3>props</h3>
     <table class="bordered responsive-table">
       <thead>
@@ -46,16 +22,67 @@
           <td>Array</td>
           <td>[]</td>
         </tr>
+        <tr>
+          <td>lane</td>
+          <td>弹幕漂流的Y坐标</td>
+          <td>Array</td>
+          <td>[]</td>
+        </tr>
+        <tr>
+          <td>done</td>
+          <td>弹幕结束的回掉</td>
+          <td>function</td>
+          <td>[]</td>
+        </tr>
       </tbody>
     </table>
     <code>
-      &lt;iBarrage :list="[{<br>          txt: '~我卡咔咔',<br>          fontSize: '11px',<br>          color: '#000',<br>          offsetX: 0,<br>          offsetY: 16,<br>          speed: 1,<br>          time: 1<br>        }, {<br>          txt: '你踩踩~',<br>          fontSize: '11px',<br>          color: '#000',<br>          offsetX: 32,<br>          offsetY: 30,<br>          speed: 1,<br>          time: 2<br>        }, {<br>          txt: '我是弹幕~',<br>          fontSize: '11px',<br>          color: '#000',<br>          offsetX: 80,<br>          offsetY: 100,<br>          speed: 2,<br>          time: Infinity<br>        }]"&gt;&lt;/iBarrage&gt;
+      &lt;iBarrage :list="[{<br>          txt: '~我卡咔咔',<br>          fontSize: '11px',<br>          color: '#000',<br>          speed: 1,<br>          time: 1<br>        }, {<br>          txt: '你踩踩~',<br>          fontSize: '11px',<br>          color: '#000',<br>          speed: 1,<br>          time: 2<br>        }, {<br>          txt: '我是弹幕~',<br>          fontSize: '11px',<br>          color: '#000',<br>          speed: 2,<br>          time: Infinity<br>        }]" :lane="[100,10,250]" @done="doneBarrage"&gt;&lt;/iBarrage&gt;·
     </code>
   </article>
 </template>
 <script>
   export default {
-    name: 'i-barrage'
+    name: 'i-barrage',
+    data () {
+      return {
+        list: [{
+          txt: '~我卡咔咔',
+          fontSize: '11px',
+          color: '#000',
+          speed: 1,
+          time: 1
+        }, {
+          txt: '你踩踩~',
+          fontSize: '11px',
+          color: '#000',
+          speed: 1,
+          time: 2
+        }, {
+          txt: '我是弹幕~',
+          fontSize: '11px',
+          color: '#000',
+          speed: 2,
+          time: Infinity
+        }]
+      }
+    },
+    methods: {
+      randomTxt () {
+        return eval(`'\\u${(Math.round(Math.random() * 20901) + 19968).toString(16)}'`)  //eslint-disable-line
+      },
+      doneBarrage (item, idx) {
+        console.log('done')
+        this.list.splice(idx, 1)
+        this.list.push({
+          txt: `${this.randomTxt()}${this.randomTxt()}${this.randomTxt()}${this.randomTxt()}`,
+          fontSize: '11px',
+          color: '#000',
+          speed: (Math.random() + 1),
+          time: 1
+        })
+      }
+    }
   }
 
 </script>
--- src/components/IBarrage/index.vue
+++ src/components/IBarrage/index.vue
@@ -3,8 +3,17 @@
 </template>
 <script>
   export default {
+    data () {
+      return {
+        laneObj: undefined
+      }
+    },
     props: {
       list: {
+        type: Array,
+        default: []
+      },
+      lane: {
         type: Array,
         default: []
       },
@@ -20,12 +29,51 @@
       this.ctx = this.$el.getContext('2d')
       this.storyBorad()
     },
+    destroyed () {
+      // this.list.map(item => {
+      //   item.offsetX = undefined
+      //   item.offsetY = undefined
+      // })
+    },
     methods: {
       _clearCtx () {
         this.ctx.clearRect(0, 0, this.$el.width, this.$el.height)
       },
-      _drawfont (item) {
+      // 随机泳道
+      _random () {
+        this.laneObj.sort(() => Math.random() - 0.5)
+        let idx = Math.floor(Math.random() * Math.pow(10, 2)) * (Date.now()) % this.laneObj.length
+        return this.laneObj.splice(idx, 1)[0]
+      },
+      // 初始化
+      _init (item) {
         if (item.time <= 0) return
+        if (this.laneObj === undefined) {
+          this.laneObj = this.lane.map((v, idx) => idx)
+        } else if (this.laneObj.length === 0) {
+          return
+        }
+        // 当销毁后判断泳道是否已经有人
+        if (item.laneIdx !== undefined) {
+          var idx = this.laneObj.indexOf(item.laneIdx)
+          if (~idx) {
+            this.laneObj.splice(idx, 1)
+          }
+        }
+        if (item.offsetX === undefined) {
+          item.offsetX = parseInt(Math.random() * (10 + 1))
+        }
+        if (item.offsetY === undefined) {
+          item.laneIdx = this._random()
+          item.offsetY = this.lane[item.laneIdx]
+        }
+        if (item.x == null) {
+          item.x = this.$el.width + item.offsetX
+        }
+      },
+      // 画字
+      _drawfont (item) {
+        if (item.time <= 0 || item.offsetY === undefined) return
         this.ctx.save()
         this.ctx.textBaseline = 'middle'
         this.ctx.textAlign = this.direction
@@ -35,26 +83,29 @@
         this.ctx.fillText(item.txt, 0, item.offsetY)
         this.ctx.restore()
       },
-      _changePos (item) {
-        if (item.x == null) {
-          // 第一次
-          item.x = this.$el.width + item.offsetX
-        } else {
-          // 是否到头
-          this.ctx.save()
-          this.ctx.font = `${item.fontSize} ETrump-QiHei55`
-          var needW = this.ctx.measureText(item.txt).width
-          this.ctx.restore()
-          if (item.x < 0 && Math.abs(item.x) >= needW) {
-            // 重新开始
-            item.x = this.$el.width
-            item.time -= 1
-          }
+      // 改变坐标
+      _changePos (item, idx) {
+        if (item.offsetY === undefined) return
+        // 是否到头
+        this.ctx.save()
+        this.ctx.font = `${item.fontSize} ETrump-QiHei55`
+        var needW = this.ctx.measureText(item.txt).width
+        this.ctx.restore()
+        if (item.x < 0 && Math.abs(item.x) >= needW) {
+          // 重新开始
+          item.x = this.$el.width
+          item.time -= 1
+        }
+        if (item.time === 0) {
+          this.laneObj.push(item.laneIdx)
+          item.offsetY = undefined
+          this.$emit('done', item, idx)
         }
         // 开始移动
         item.x = item.x - item.speed
       },
       _requestAnimationFrame (callback) {
+        if (this._isDestroyed) return
         if (/android|adr/gi.test(navigator.userAgent)) {
           setTimeout(callback, 1000 / 60)
         } else {
@@ -71,6 +122,7 @@
       storyBorad () {
         this._clearCtx()
         if (this.list.length) {
+          this.list.map(this._init)
           this.list.map(this._changePos)
           this.list.map(this._drawfont)
         }
