4abe7f1009c95d3650d514de8ce7da140e55a2ff
maifei <maifei@in66.com>
2017-03-16 18:30:57 +0800

    Revert "抽出路由配置"
    This reverts commit b831968c22511bd0e34b27b7109fad39514f9dcd.

--- example/routes.js
+++ example/routes.js
@@ -1,26 +1,26 @@
-/**
- * Icon by https://www.iviewui.com/components/icon
- */
-import navConfig from './nav.config'
+import install from './md/install.md'
+import quickstart from './md/quickstart.md'
+import customPackage from './md/custom-package.md'
 
-let routes = []
-let docs = ['/install', '/quickstart', '/custom-package']
+import iAudio from 'i-ui/lib/iAudio/README.md'
+import iAvatar from 'i-ui/lib/iAvatar/README.md'
+import IBarrage from 'i-ui/lib/IBarrage/README.md'
+import iImgUpload from 'i-ui/lib/iImgUpload/README.md'
+import iRollBar from 'i-ui/lib/iRollBar/README.md'
+import iTrack from 'i-ui/lib/iTrack/README.md'
 
-navConfig.forEach(navs => {
-  if (!navs.children) return;
-  navs.children.forEach(nav => {
-    routes.push({
-      path: nav.path,
-      component: docs.indexOf(nav.path) >= 0 ?
-        require(`./md${nav.path}.md`) :
-        require(`../packages${nav.path}/README.md`)
-    })
-  })
-})
+// https://www.iviewui.com/components/icon
 
-routes.push({
-  path: '/',
-  redirect: '/install'
-})
-
+const routes = [
+  {path: '/', redirect: '/install'},
+  {name: 'install', path: '/install', component: install },
+  {name: 'customPackage', path: '/customPackage', component: customPackage },
+  {name: 'quickstart', path: '/quickstart', component: quickstart },
+  {name: 'iAudio', path: '/iAudio', component: iAudio, type: 'components', icon: 'music-note', txt: '音乐播放'},
+  {name: 'iAvatar', path: '/iAvatar', component: iAvatar, type: 'components', icon: 'social-reddit', txt: '头像'},
+  {name: 'IBarrage', path: '/IBarrage', component: IBarrage, type: 'components', icon: 'android-textsms', txt: '弹幕'},
+  {name: 'iImgUpload', path: '/iImgUpload', component: iImgUpload, type: 'components', icon: 'image', txt: '图片选择'},
+  {name: 'iRollBar', path: '/iRollBar', component: iRollBar, type: 'components', icon: 'android-more-vertical', txt: '滚动加载'},
+  {name: 'iTrack', path: '/iTrack', component: iTrack, type: 'directives', icon: 'social-rss', txt: '埋点'}
+]
 export default routes
