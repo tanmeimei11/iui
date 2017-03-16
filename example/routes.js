99a4f19a75955d91f8f4a95d0e97c22a4700a56e
maifei <maifei@in66.com>
2017-03-16 18:21:20 +0800

    modified:   example/routes.js

--- example/routes.js
+++ example/routes.js
@@ -3,17 +3,37 @@
  */
 import navConfig from './nav.config'
 
+import install from './md/install.md'
+import quickstart from './md/quickstart.md'
+import customPackage from './md/custom-package.md'
+
+import iAudio from 'i-ui/lib/iAudio/README.md'
+import iAvatar from 'i-ui/lib/iAvatar/README.md'
+import IBarrage from 'i-ui/lib/IBarrage/README.md'
+import iImgUpload from 'i-ui/lib/iImgUpload/README.md'
+import iRollBar from 'i-ui/lib/iRollBar/README.md'
+import iTrack from 'i-ui/lib/iTrack/README.md'
+
+let map = {
+    install,
+    quickstart,
+    'custom-package': customPackage,
+    iAudio,
+    iAvatar,
+    IBarrage,
+    iImgUpload,
+    iRollBar,
+    iTrack
+}
+
 let routes = []
-let docs = ['/install', '/quickstart', '/custom-package']
 
 navConfig.forEach(navs => {
   if (!navs.children) return;
   navs.children.forEach(nav => {
     routes.push({
       path: nav.path,
-      component: docs.indexOf(nav.path) >= 0 ?
-        require(`./md${nav.path}.md`) :
-        require(`../packages${nav.path}/README.md`)
+      component: map[nav.path.replace('/', '')]
     })
   })
 })
