f8e49d7ed8c02c78dbd76b77536bf2da49812578
jiuyan <jiuyan@jiuyandeMacBook-Pro.local>
2017-04-17 15:30:16 +0800

    qianyi

---
+++ packages/iFireWorks/src/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container" id="container">
+  </div>
+</template>
+<script>
+  import draw from './draw.js'
+  export default {
+    name: 'iFireWorks',
+    props: [
+      'imgUrl'
+    ],
+    watch: {
+      imgUrl (val, old) {
+        draw.reDraw(val)
+      }
+    },
+    mounted () {
+      draw.init(this.$el)
+    },
+    created () {}
+  }
+
+</script>
+<style>
+  .container {
+    width: 100%;
+    height: 300px;
+  }
+
+</style>
