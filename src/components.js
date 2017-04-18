2c8e83a0c36da936404076888414b02ad39247df
diandi <diandi@in66.com>
2017-04-18 17:37:32 +0800

    add new version

--- src/components.js
+++ src/components.js
@@ -5,11 +5,13 @@ import iAudio from '../packages/iAudio'
 import iAvatar from '../packages/iAvatar'
 import IBarrage from '../packages/IBarrage'
 import iImgUpload from '../packages/iImgUpload'
+import iImgCut from '../packages/iImgCut'
 import iRollBar from '../packages/iRollBar'
 import iSharePanel from '../packages/iSharePanel'
 import iVideoScreen from '../packages/iVideoScreen'
 import iTrack from '../packages/iTrack'
 import iOpen from '../packages/iOpen'
+import iBgImg from '../packages/iBgImg'
 
 const install = function (Vue, opts = {}) {
   if (install.installed) return
@@ -18,11 +20,13 @@ const install = function (Vue, opts = {}) {
   Vue.component(iAvatar.name, iAvatar)
   Vue.component(IBarrage.name, IBarrage)
   Vue.component(iImgUpload.name, iImgUpload)
+  Vue.component(iImgCut.name, iImgCut)
   Vue.component(iRollBar.name, iRollBar)
   Vue.component(iSharePanel.name, iSharePanel)
   Vue.component(iVideoScreen.name, iVideoScreen)
   Vue.directive(iTrack.name, iTrack)
   Vue.directive(iOpen.name, iOpen)
+  Vue.directive(iBgImg.name, iBgImg)
 }
 
 /* istanbul ignore if */
@@ -31,15 +35,17 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '0.1.16',
+  version: '0.1.17',
   install,
   iAudio,
   iAvatar,
   IBarrage,
   iImgUpload,
+  iImgCut,
   iRollBar,
   iSharePanel,
   iVideoScreen,
   iTrack,
-  iOpen
+  iOpen,
+  iBgImg
 }
