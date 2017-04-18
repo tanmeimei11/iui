c3c52565362d14f0d5061e52806cf0c718ca56fe
diandi <diandi@in66.com>
2017-04-18 17:34:09 +0800

    fix unit error

--- test/unit/specs/componets/iBgImg.spec.js
+++ test/unit/specs/componets/iBgImg.spec.js
@@ -13,17 +13,17 @@ const Ctor = Vue.extend({
   template: '<div class="sample" v-iBgImg="coverUrl"></div>'
 })
 const vm = new Ctor().$mount()
-const parentEl = vm.$el
+const parentEl = vm.$el 
 describe('iBgImg', () => {
   it('iBgImg组件是否正常显示', () => {
-    expect(parentEl.style.backgroundImage).to.equal(`url("${vm.coverUrl.replace(/https?:/, location.protocol)}")`)
+    expect(parentEl.style.backgroundImage).to.contain(`${vm.coverUrl.replace(/https?:/, location.protocol)}`)
   })
   it('iBgImg组件动态更新数据', (done) => {
     vm.coverUrl = `http://inimg01.jiuyan.info/in/2015/12/06/50E0D236-8186-87D4-278E-184399C6B81D-1ndoVWb.jpg`
     setTimeout(() => {
-      expect(parentEl.style.backgroundImage).to.equal(`url("${vm.coverUrl.replace(/https?:/, location.protocol)}")`)
+      expect(parentEl.style.backgroundImage).to.contain(`${vm.coverUrl.replace(/https?:/, location.protocol)}`)
       done()
-    }) 
+    })  
   })
   it('iBgImg组件cover属性', () => {
     const CoverCtor = Vue.extend({
