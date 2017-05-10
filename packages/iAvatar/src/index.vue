5146c88c9ff4e372c41fe1d9070384360c0d4f40
jiuyan <jiuyan@jiuyandeMacBook-Pro.local>
2017-05-10 17:23:08 +0800

    add

--- packages/iAvatar/src/index.vue
+++ packages/iAvatar/src/index.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="i-avatar" :style="{'background-image': `url(${avatar})`}"></div>
+  <div class="i-avatar" :style="{'background-image': `url(${avatar})`}" @click="avatarClick"></div>
 </template>
 <script>
+  import comm from 'i-ui/src/utils/common.js'
+  import awake from 'i-ui/src/utils/jsBridge.js'
   export default {
     name: 'iAvatar',
     props: {
-      src: {type: String, required: true}
+      src: {
+        type: String,
+        required: true
+      },
+      uid: {
+        type: String,
+        required: true
+      }
     },
     computed: {
       avatar () {
@@ -15,12 +24,21 @@
         anchor.protocol = location.protocol
         return anchor.href
       }
+    },
+    methods: {
+      avatarClick () {
+        if (comm.isInApp) {
+          awake(`in://diary/other?userid=${this.uid}`)
+        } else {
+          location.href = `${location.protocol}//h5.in66.com/inpromo/inweb/in-share/in.html?uid=${this.uid}`
+        }
+      }
     }
   }
 
 </script>
 <style lang="scss">
-  .i-avatar{
+  .i-avatar {
     width: 40px;
     height: 40px;
     border-radius: 50%;
@@ -28,4 +46,5 @@
     background-repeat: no-repeat;
     background-size: cover;
   }
+
 </style>
