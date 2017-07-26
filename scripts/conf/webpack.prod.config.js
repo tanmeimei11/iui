4dc49515af780aa74ea71401dd59f46299f3abc6
diandi <diandi@in66.com>
2017-07-26 10:53:00 +0800

    add webpack DllPlugin

--- scripts/conf/webpack.prod.config.js
+++ scripts/conf/webpack.prod.config.js
@@ -2,14 +2,21 @@ import webpack from 'webpack'
 import Config from 'webpack-config'
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
-import { env } from './utils'
+import { env, resolvePath } from './utils'
 import components from '../../components.json'
 import directives from '../../directives.json'
 
-const cfg = new Config().extend('scripts/conf/webpack.base.config.js').merge({
-  entry: components
-}).merge({
-  entry: directives
+const cfg = new Config().extend({
+  'scripts/conf/webpack.base.config.js': config => {
+    config.entry = config.entry || {}
+    Object.keys(components).forEach(key => {
+      config.entry[`${key}`] = [components[`${key}`]]
+    })
+    Object.keys(directives).forEach(key => {
+      config.entry[`${key}`] = [directives[`${key}`]]
+    })
+    return config
+  }
 }).merge({
   entry: {
     'i-ui.common': ['./src/components.js'],
@@ -29,6 +36,11 @@ const cfg = new Config().extend('scripts/conf/webpack.base.config.js').merge({
     }),
     new ExtractTextPlugin({ 
       filename: getPath => getPath('theme-default/[name].css').replace('i-ui.common', 'index')
+    }),
+    new webpack.DllPlugin({
+      path: resolvePath('lib/[name].manifest.json'),
+      name: '[name]',
+      context: resolvePath('')
     })
   ]
 })
@@ -38,5 +50,4 @@ if (env.bundleAnalyzerReport) {
     plugins: [new BundleAnalyzerPlugin()]
   })
 }
-
 export default cfg
