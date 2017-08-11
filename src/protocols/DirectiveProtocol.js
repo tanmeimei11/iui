b656f2074bdbfb437c58762b450f82114851dd17
maifei <maifei@in66.com>
2017-08-11 17:10:43 +0800

    modified:   src/protocols/DirectiveProtocol.js

--- src/protocols/DirectiveProtocol.js
+++ src/protocols/DirectiveProtocol.js
@@ -4,27 +4,27 @@ import common from 'i-ui/src/utils/common'
 
 export default class DirectiveProtocol extends Protocol {
   static _unbind = function (el, binding, _v) {
-    el.removeEventListener('touchstart', el[`${this.directiveKey}`])
+    el.removeEventListener('click', el[`${this.directiveKey}`])
   }
 
   static _directive = function (el, binding, _v) {
-    let source = new this(binding.value) 
-      el.removeEventListener('touchstart', el[`${this.directiveKey}`])
+    let source = new this(binding.value)
+      el.removeEventListener('click', el[`${this.directiveKey}`])
       el[`${this.directiveKey}`] = () => {
         if (binding.modifiers.protocol) {
           awake(source.protocol)
         } else if (binding.modifiers.href) {
-          location.href = source.link 
+          location.href = source.link
         } else {
           if (common.isInApp) {
             awake(source.protocol)
           } else {
-            location.href = source.link 
-          } 
+            location.href = source.link
+          }
         }
 
       }
-    el.addEventListener('touchstart', el[`${this.directiveKey}`])
+    el.addEventListener('click', el[`${this.directiveKey}`])
   }
 
   static directive = function (clazz) {
