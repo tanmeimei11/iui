8768a7c7a657f31c3ce697ce54fbe7adb4c72920
diandi <diandi@in66.com>
2017-08-21 11:44:46 +0800

    remove irollbar svg

--- packages/iRollBar/src/iRollBar.vue
+++ packages/iRollBar/src/iRollBar.vue
@@ -16,11 +16,6 @@
         type: Boolean,
         default: true
       },
-      rollIng: {
-        require: true,
-        type: Function,
-        default: () => {}
-      },
       showBar: {
         type: String
       }
@@ -59,32 +54,9 @@
       }
     },
     render (createElement) {
-      const data = {
+      return createElement(this.tag, {
         class: `i-roll-bar ${(this.showBar === undefined) ? 'hiddenBar' : ''}`
-      }
-      let child = this.$slots.default || []
-      if (this.hasMore) {
-        child.push(createElement('div', {
-          attrs: {
-            class: 'i-rollbar-loading-svg'
-          },
-          domProps: {
-            innerHTML: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><circle transform="translate(8 0)" cx="0" cy="16" r="0"> 
-              <animate attributeName="r" values="0; 4; 0; 0" dur="1.2s" repeatCount="indefinite" begin="0"
-                keytimes="0;0.2;0.7;1" keySplines="0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.6 0.4 0.8" calcMode="spline" />
-            </circle>
-            <circle transform="translate(16 0)" cx="0" cy="16" r="0"> 
-              <animate attributeName="r" values="0; 4; 0; 0" dur="1.2s" repeatCount="indefinite" begin="0.3"
-                keytimes="0;0.2;0.7;1" keySplines="0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.6 0.4 0.8" calcMode="spline" />
-            </circle>
-            <circle transform="translate(24 0)" cx="0" cy="16" r="0"> 
-              <animate attributeName="r" values="0; 4; 0; 0" dur="1.2s" repeatCount="indefinite" begin="0.6"
-                keytimes="0;0.2;0.7;1" keySplines="0.2 0.2 0.4 0.8;0.2 0.6 0.4 0.8;0.2 0.6 0.4 0.8" calcMode="spline" />
-            </circle></svg>`
-          }
-        }))
-      }
-      return createElement(this.tag, data, child)
+      }, this.$slots.default)
     }
   }
 
@@ -95,11 +67,6 @@
     // overflow: auto;
     &.hiddenBar::-webkit-scrollbar {
       display: none;
-    }
-    svg {
-      width: 100%;
-      height: .42rem;
-      fill: #f54;
     }
   }
 
